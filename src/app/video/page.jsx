140c2f2bebfff3736f355622e747d5ea54337cde
Tan Heng Wai <mushroomweii@gmail.com>
2024-05-18 17:19:26 +0800

    add video page

---
+++ src/app/video/page.jsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+
+function getVideo() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('get video success');
+    }, 5000);
+  });
+}
+
+export const maxDuration = 15;
+
+async function VideoPageResult() {
+  const videoMessage = await getVideo();
+
+  return (
+    <div>
+      result: {videoMessage}
+    </div>
+  )
+}
+
+export default async function VideoPage() {
+  
+  return (
+    <div>
+      <h1>
+        hi this is video page
+      </h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <VideoPageResult />
+      </Suspense>
+    </div>
+  )
+}
